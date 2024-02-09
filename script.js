9da5518cae76455196c3bb540c2fc1d5d1975b4c
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-02-09 18:56:33 +0000

    basic colours

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('test')
+const btn = document.getElementById('btn');
+const colorLabel = document.getElementById('colorLabel');
+
+const colours = [
+    "Silver",
+    "Gray",
+    "Maroon",
+    "Red",
+    "Purple",
+    "Fuchsia",
+    "Green",
+    "Lime",
+    "Yellow",
+    "Olive",
+    "Blue",
+    "Navy",
+    "Teal",
+    "Aqua"
+]
+
+btn.addEventListener('click', colourChange);
+
+function colourChange() {
+    randomNumber = Math.floor(Math.random() * (colours.length));
+    document.body.style.backgroundColor = colours[randomNumber];
+    colorLabel.innerText = `Background Color : ${colours[randomNumber]}`;
+    console.log(randomNumber)
+}
