68c853f885a9be09e90447e7410f9b522fb70401
MonikaCiesielska <150713260+MonikaCie@users.noreply.github.com>
2024-02-10 13:02:25 +0000

    rgb colours

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const colours = [
     "Aqua"
 ]
 
-btn.addEventListener('click', colourChange);
+btn.addEventListener('click', colourRGB);
 
 function colourChange() {
     randomNumber = Math.floor(Math.random() * (colours.length));
@@ -26,3 +26,17 @@ function colourChange() {
     colorLabel.innerText = `Background Color : ${colours[randomNumber]}`;
     console.log(randomNumber)
 }
+
+
+function colourRGB() {
+    let randonRed = Math.floor(Math.random() * 255);
+    let randomGreen = Math.floor(Math.random() * 255);
+    let randomBlue = Math.floor(Math.random() * 255);
+    let RGB = `rgb(${randonRed},${randomGreen},${randomBlue})`
+    console.log(RGB);
+    document.body.style.backgroundColor = RGB;
+    colorLabel.innerText = `Background Color : ${RGB}`;
+
+}
+
+colourRGB()
